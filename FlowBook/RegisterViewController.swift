fdbf71f501f28aa38e3c6736706dc366b4d55511
BenAfonso <darkyler57@gmail.com>
2017-02-11 22:03:24 +0100

    feat<RegisterVC>: Form control and error messages

--- FlowBook/RegisterViewController.swift
+++ FlowBook/RegisterViewController.swift
@@ -8,39 +8,31 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    @IBOutlet weak var repeatPasswordErrorLabel: UILabel!
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        self.passwordTextField.addTarget(self, action: #selector(checkPasswordFieldOnChange(_:)), for: .editingChanged)
+        self.repeatPasswordTextField.addTarget(self, action: #selector(checkRepeatPasswordFieldOnChange(_:)), for: .editingChanged)
+        self.emailTextField.addTarget(self, action: #selector(checkEmailFieldOnChange(_:)), for: .editingChanged)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-
-    @IBAction func RegisterAction(_ sender: Any) {
-        // Save user into database
-    }
-    
-    @IBAction func cancelAction(_ sender: Any) {
-        self.clearForm()
-    }
-
-    
-    func clearForm() {
-        self.emailTextField.text = ""
-        self.firstNameTextField.text = ""
-        self.lastNameTextField.text = ""
-        self.passwordTextField.text = ""
-        self.repeatPasswordTextField.text = ""
-    }
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,6 +41,151 @@ class RegisterViewController: UIViewController {
     
 
     
+    // MARK: Actions 
+    
+    @IBAction func RegisterAction(_ sender: Any) {
+        
+        guard let email: String = emailTextField.text,
+            let firstName: String = firstNameTextField.text,
+            let lastName: String = lastNameTextField.text,
+            let password: String = passwordTextField.text,
+            let repeatPassword: String = repeatPasswordTextField.text else {
+                return
+        }
+        
+        // Checks for empty fields
+        guard email != "",
+            firstName != "",
+            lastName != "",
+            password != "",
+            repeatPassword != "" else {
+                self.showError(withMessage: "Les champs ne peuvent pas être vides.")
+                return
+        }
+        
+        guard password.characters.count > 6 else {
+            self.showError(withMessage: "Le mot de passe doit contenir au moins 6 caractère.")
+            return
+        }
+        
+        guard email.contains("@") else {
+            self.showError(withMessage: "Veuillez renseigner une adresse email valide.")
+            return
+        }
+        
+        guard password == repeatPassword else {
+            
+            self.showError(withMessage: "Les deux mots de passes ne sont pas identiques.")
+            // UIAlert HERE
+            self.clearPasswordField()
+            return
+        }
+        
+        
+        // Save user into CoreData
+        do {
+            let newUser: User = try User.create(withFirstName: firstName,
+                                                withLastName: lastName,
+                                                withEmail: email,
+                                                withPassword: password)
+            self.clearForm()
+            performSegue(withIdentifier: "registerSuccess", sender: self)
+            print(newUser)
+        } catch let error as NSError {
+            //UIAlert HERE
+            
+            self.showError(withMessage: "Erreur lors de l'enregistrement du compte.")
+            print(error)
+            return
+        }
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        self.clearForm()
+    }
+    
+    // MARK: Check fields values
+    
+    func checkPasswordFieldOnChange(_ textField: UITextField) {
+        if textField == self.passwordTextField {
+            if !self.checkPasswordValid(password: self.passwordTextField.text!) {
+                passwordErrorLabel.isHidden = false
+                passwordErrorLabel.text = "Le mot de passe doit contenir au moins 6 caractères."
+            } else {
+                passwordErrorLabel.isHidden = true
+            }
+        }
+    }
+    
+    func checkRepeatPasswordFieldOnChange(_ textField: UITextField) {
+            if !self.checkPasswords(password1: self.passwordTextField.text!, password2: self.repeatPasswordTextField.text!) {
+                
+                repeatPasswordErrorLabel.isHidden = false
+                repeatPasswordErrorLabel.text = "Les mots de passe ne sont pas identiques."
+            } else {
+                repeatPasswordErrorLabel.isHidden = true
+            }
+    }
+    
+    func checkEmailFieldOnChange(_ textField: UITextField) {
+        if textField == self.emailTextField {
+            if !self.checkEmailValid(email: self.emailTextField.text!) {
+                emailErrorLabel.isHidden = false
+                emailErrorLabel.text = "L'adresse email n'est pas valide."
+            } else {
+                emailErrorLabel.isHidden = true
+            }
+        }
+    }
+    
+
+    // MARK: Check vlaues validity
+    
+    func checkEmailValid(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,20}"
+        let emailTest  = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func checkPasswordValid(password: String) -> Bool {
+        return password.characters.count >= 6
+    }
+    
+    func checkPasswords(password1: String, password2: String) -> Bool {
+        return password1 == password2
+    }
+    
+    
+    
+    // MARK: User interactions 
+    
+    func showError(withMessage message: String) {
+        self.errorMessageLabel.isHidden = false
+        self.errorMessageLabel.text = message
+    }
+    
+    
+    /// Clears the form resetting all textfields to ""
+    func clearForm() {
+        self.emailTextField.text = ""
+        self.firstNameTextField.text = ""
+        self.lastNameTextField.text = ""
+        self.passwordTextField.text = ""
+        self.repeatPasswordTextField.text = ""
+        self.hideErrorLabels()
+    }
+    
+    func clearPasswordField() {
+        self.passwordTextField.text = ""
+        self.repeatPasswordTextField.text = ""
+        self.hideErrorLabels()
+    }
+    
+    func hideErrorLabels() {
+        self.emailErrorLabel.isHidden = true
+        self.passwordErrorLabel.isHidden = true
+        self.repeatPasswordErrorLabel.isHidden = true
+    }
     
     
 }
--- FlowBook/UserExtension.swift
+++ FlowBook/UserExtension.swift
@@ -7,7 +7,37 @@
 //
 
 import Foundation
-
+import UIKit
 extension User {
     
+    
+    static func create(withFirstName firstName: String,
+                withLastName lastName: String,
+                withEmail email: String,
+                withPassword password: String) throws -> User {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            throw NSError()
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let user = User(context: context)
+        user.lastName = lastName
+        user.firstName = firstName
+        user.email = email
+        user.password = password
+        
+        do {
+            try context.save()
+            return user
+        }
+        catch let error as NSError {
+            throw error
+        }
+    }
+    
+    func save(error: () -> ()) {
+        
+    }
 }
