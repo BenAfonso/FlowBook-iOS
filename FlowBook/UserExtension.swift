20304572791bca90a0221ea1fb57793da0478080
BenAfonso <darkyler57@gmail.com>
2017-02-11 02:54:23 +0100

    Starting models

---
+++ FlowBook/UserExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UserExtension.swift
+//  FlowBook
+//
+//  Created by Benjamin Afonso on 11/02/2017.
+//  Copyright © 2017 Benjamin Afonso. All rights reserved.
+//
+
+import Foundation
+
+extension User {
+    
+}
