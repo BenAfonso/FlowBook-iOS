68bd0aa37afa2a1e173827771aab2cda261d7fe8
bastienricoeur <bastienricoeur@gmail.com>
2017-03-07 08:52:59 +0100

    update mainstoryboard

---
+++ FlowBook/NewEventViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewEventViewController.swift
+//  FlowBook
+//
+//  Created by Bastien Ricoeur on 07/03/2017.
+//  Copyright © 2017 Benjamin Afonso. All rights reserved.
+//
+
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
